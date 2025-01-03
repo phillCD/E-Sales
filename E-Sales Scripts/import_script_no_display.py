f6e813b8cec38a626bb4a25be6c4b2b09720e946
phillCD <fil.milaneze@gmail.com>
2025-01-03 11:56:42 -0300

    main: added import script

---
+++ E-Sales Scripts/import_script_no_display.py
@@ -0,0 +1,89 @@
+import base64
+import os
+import tempfile
+from itertools import product
+
+import openpyxl
+import requests
+
+box_url = 'http://localhost:8080/product-box'
+
+product_url = 'http://localhost:8080/products'
+
+product_to_api = {
+    'name': '',
+    'reference': '',
+    'brand': '',
+    'ncm': '',
+    'cest': '',
+    'ipi': '',
+    'barcode': '',
+    'height': '',
+    'width': '',
+    'weight': '',
+    'length': '',
+    'price': '',
+}
+
+box_to_api = {
+    'product': {},
+    'box_barcode': '',
+    'box_weight': '',
+    'box_height': '',
+    'box_width': '',
+    'box_length': '',
+    'box_quantity': '',
+}
+
+display_to_api = {
+    'product': {},
+    'display_barcode': '',
+    'display_weight': '',
+    'display_height': '',
+    'display_width': '',
+    'display_length': '',
+    'display_quantity': '',
+}
+
+def import_product(data):
+    base64_data = data['file']
+    decoded_data = base64.b64decode(base64_data)
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as temp_file:
+        temp_file.write(decoded_data)
+        temp_file_path = temp_file.name
+
+    wb = openpyxl.load_workbook(temp_file_path)
+    sheet = wb.active
+
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        if any(cell is not None for cell in row):
+            product_to_api['name'] = row[1]
+            product_to_api['reference'] = row[0]
+            product_to_api['brand'] = row[2]
+            product_to_api['price'] = row[3]
+            product_to_api['ipi'] = row[4]
+            product_to_api['ncm'] = row[5]
+            product_to_api['cest'] = row[6]
+            product_to_api['barcode'] = row[7].replace('\xa0', '') if row[7] else ''
+            product_to_api['length'] = row[8]
+            product_to_api['width'] = row[9]
+            product_to_api['height'] = row[10]
+            product_to_api['weight'] = row[11]
+            box_to_api['box_barcode'] = row[12].replace('\xa0', '') if row[12] else ''
+            box_to_api['box_length'] = row[13]
+            box_to_api['box_width'] = row[14]
+            box_to_api['box_height'] = row[15]
+            box_to_api['box_weight'] = row[16]
+            box_to_api['box_quantity'] = row[17]
+            print(product_to_api)
+            print(box_to_api)
+
+            response = requests.post(product_url, json=product_to_api)
+            product_to_box = response.json()
+            box_to_api['product'] = product_to_box
+            response_box = requests.post(box_url, json=box_to_api)
+
+            print(response_box.json())
+            print(response.json())
+
+    os.remove(temp_file_path)
